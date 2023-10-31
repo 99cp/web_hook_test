9e9480df5e35963299f2cd549bf7c738ca8366fa
99cp <103494820+99cp@users.noreply.github.com>
2023-10-31 12:11:22 +0900

    image 삭제 후 테스트

--- src/main/java/com/example/cpuboundapp/controller/AppController.java
+++ src/main/java/com/example/cpuboundapp/controller/AppController.java
@@ -20,7 +20,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook docker test + build test + build test no sudo message + socket permission + dockerhub upload1";
+        return "webhook docker test + build test + build test no sudo message + socket permission + dockerhub upload1 + one more check";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
