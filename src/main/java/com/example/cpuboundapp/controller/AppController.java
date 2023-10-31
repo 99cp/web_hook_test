412084ba6a64f81126791299c76187e419fc977c
99cp <103494820+99cp@users.noreply.github.com>
2023-10-31 11:23:25 +0900

    socket permission

--- src/main/java/com/example/cpuboundapp/controller/AppController.java
+++ src/main/java/com/example/cpuboundapp/controller/AppController.java
@@ -20,7 +20,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook docker test + build test!!";
+        return "webhook docker test + build test + build test no sudo message + socket permission";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
