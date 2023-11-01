267e685fc8df2dfb5d31bd3d12d354b0e21f2d2c
99cp <103494820+99cp@users.noreply.github.com>
2023-11-01 12:18:22 +0900

    unstable > success test

--- src/main/java/com/example/cpuboundapp/controller/AppController.java
+++ src/main/java/com/example/cpuboundapp/controller/AppController.java
@@ -20,7 +20,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook test restart!!12!!! + sleep 60 + instance 3,4 only22";
+        return "webhook test restart!!12!!! + sleep 60 + instance 3,4 only2ss2";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
